223cebb5d80716ecbcc529816aae1b3a4c0a000e
Dharaneesh guhan <dharaneeshguhan.t2023ece@sece.ac.in>
2025-07-15 09:22:36 +0530

    day-10

---
+++ day-10/Springbootfirstproject/src/main/java/com/example/project/Repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.project.Repository;
+
+import com.example.Springbootfirstproject.Models.RegisterDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<RegisterDetails,Integer> {
+    RegisterDetails findByEmail(String email);
+
+}
